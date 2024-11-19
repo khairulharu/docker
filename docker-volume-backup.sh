930ea5438f63bb6f7b457aab5d0fa5b9b83a6b99
khairulharu <khairulaswad719@gmail.com>
2024-11-19 15:46:00 +0800

    bacup volume data

--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -8,6 +8,14 @@ docker container exec -i -t nginxbackup /bin/bash
 
 tar cvf /backup/backup.tar.gz /data
 
+docker container stop nginxbacup
+
 docker container rm nginxbackup
+
+docker container start mongovolume
+
+docker image pull ubuntu:latest
+
+docker container run --rm --name ubuntubackup --mount "type=bind,source=/home/khairulaswad/Documents/learn-docker/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" ubuntu:latest tar cvf /backup/backup-ubuntu.tar.gz /data
 
 docker container start mongovolume
